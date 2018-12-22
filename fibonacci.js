8d83b523bf1fe64859b1b51cbafaa9fbca338924
James868 <rizkykho15@gmail.com>
2018-12-22 12:44:35 +0700

    Optimizing fibonacci code

--- fibonacci.js
+++ fibonacci.js
@@ -1,32 +1,6 @@
 // Output fibonacci number until index n
 
-
-
 // Iterative Solution V1 - Forward - O(n)
-
-function fib(n) {
-    const arr = [0, 1];
-    const mapper = {};
-    let counter = 0;
-    
-    for (let i = 2; i <= n; i++) {
-        let a = arr[i - 1]; 
-        let b = arr[i - 2];
-        arr.push(a + b);
-    }
-    
-    for (let elem in arr) {
-        mapper[counter] = arr[elem];
-        counter++;
-    }
-    
-    return mapper;
-}
-
-
-
-
-// Iterative Solution V1 Modified - Forward - O(n)
 function fib(n) {
     const arr = [0, 1];
     const mapper = {};
@@ -46,24 +20,8 @@ function fib(n) {
 
 
 
-// Iterative Solution V1 - Backward - O(n)
 
-function fib(n) {
-    if (n < 2) return console.log(n);
-    
-    let [a, b] = [0, 1];
-    
-    while (n-- > 1) {
-        [a, b] = [b, a + b];
-    }
-    
-    return b;
-}
-
-
-
-
-// Iterative Solution V2 - Forward  ((O(n))
+// Iterative solution V2 - Forward  - O(n)
 /*
 This method has you work upwards from 0 to n (instead of downwards from n to 0) 
 and store the results in three variables as you go. 
@@ -73,33 +31,11 @@ fibbonacci(n-2) three times, etc.
 */
 
 function fib(n) {
-    if (n < 2) return console.log(n);
     
     let a = 0;
     let b = 1;
     let c;
-    
-    for(let i = 2; i <= n; i++) {
-        c = a + b;
-        a = b;
-        b = c;
-    }
-    
-    return c;
-}
-
-
-
-
-// Iterative Solution V2 - Forward Mod With Array ((O(n))
-// Faster
-	
-function fib(n) {
-    
-    let a = 0;
-    let b = 1;
-    let c;
-    const arr = [0, 1]
+    const arr = [0, 1];
     
     for(let i = 2; i <= n; i++) {
         c = a + b;
@@ -115,57 +51,78 @@ function fib(n) {
 
 
 
-// Iterative V2 Solution - Backward (O(n)
+// Iterative solution V3 - Forward - O(n)
+
+function fib(n) {
+    if (n < 2) return n;
+    
+    let [a, b] = [0, 1];
+    
+    while (n-- > 1) {
+        [a, b] = [b, a + b];
+    }
+    
+    return b;
+}
+
+
+
+
+// Iterative solution V4 - Backward - O(n)
 
 function fib(n) {
     let a;
     let b = 0;
     let c = 1;
+    const arr = [0, 1];
     
     for (let i = 2; i <= n; i++) {
         a = b;
         b = c;
         c = a + b;
+        arr.push(c)n
     }
     
-    return c;
+    return arr;
 }
 
 
 
 
+// Recursive solution - O(2^n)
+/* 
+(n = 10 => 7.5ms)
+(n = 20 => 35ms)
+(n = 30 => 80ms)
+(n = 40 => 4320ms)
+(n = 45 => 46791ms)
+*/
 
-// Recursive Solution V1 (O(2^n))
+const fib = n => (n < 2) ? n : fib(n - 1) + fib(n - 2);
 
-function fib(n) {
-    if (n < 2) {
-        return n;
-    }
+
+
+
+// Recursive solution with memoization - O(2n)
+/* 
+(n = 100 => 1.8ms)
+(n = 1000 => 4.7ms)
+*/
+
+function fib(n, memo) {
+    memo = memo || {};
+ 
+    if (memo[n]) return memo[n];
     
-    return fib(n - 1) + fib(n - 2);
+    if (n < 2) return n;
+    
+    return memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
 }
 
 
 
 
-
-// Recursive Solution V2 (O(2^n))
-
-const fib = (n) => n < 2 ? n : fib(n-1) + fib(n-2);
-
-
-
-
-
-// Recursive Solution V3 (O(n))
-
-const fib = _.memoize((n)) => n < 2 ? n : fib(n-1) + fib(n-2);
-
-
-
-
-
-// Generator Solution ( O(n) when all numbers are calculated )
+// Generator solution - (O(n) when all numbers are calculated )
 
 function* fib(n) {
     [a, b] = [0, 1];
@@ -188,21 +145,8 @@ for (let i = 0; i < 10; i++) {
 
 
 
-
-// Tail Call optimization
-function fib(n, a=1, b=0) {
-    if (n === 0) {
-        return b;
-    } else {
-        return fib(n-1, b, a+b);
-    }
-}
-
-
-
-
-
 // ES6 Tail Call optimization
+
 const fib = (n, a = 1, b = 0) => (n === 0) ? b : fib(n - 1, a + b, a);
 
 
