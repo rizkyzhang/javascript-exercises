4b0fdac072d82ee066791a854c84176e7e0440e0
James868 <rizkykho15@gmail.com>
2019-03-09 20:25:27 +0700

    Moving files to w3resource folder

--- UC_First_Letter_Of_Each_Word.js
+++
@@ -1,9 +0,0 @@
-function capitalizeWord(word) {
-    return word
-                .split(" ")
-                .map(word => word[0].toUpperCase() + word.slice(1))
-                .join(" ");
-}
-
-
-
