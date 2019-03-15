ca41937d2fef574ddba885408b1f6c39e7322ab8
James868 <41568558+James868@users.noreply.github.com>
2019-03-15 21:12:42 +0700

    Add an array challenge

---
+++ Array/Map to names/solution.js
@@ -0,0 +1,7 @@
+const john = { name: "John", age: 25 };
+const pete = { name: "Pete", age: 30 };
+const mary = { name: "Mary", age: 28 };
+
+const users = [ john, pete, mary ];
+
+const names = users.map(user => user.name);
