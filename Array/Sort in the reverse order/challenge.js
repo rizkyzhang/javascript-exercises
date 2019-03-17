090126b73906a3a3fad9e76f253ff0ff78de1dc5
James868 <41568558+James868@users.noreply.github.com>
2019-03-17 08:53:08 +0700

    Add a new array challenge

---
+++ Array/Sort in the reverse order/challenge.js
@@ -0,0 +1,17 @@
+/*
+  Title:
+    Sort in the reverse order
+
+  Description:
+    
+
+  Examples:
+    let arr = [5, 2, 1, -10, 8];
+
+    // ... your code to sort it in the reverse order
+
+    alert( arr ); // 8, 5, 2, 1, -10
+
+  Link:
+    http://javascript.info/task/sort-back 
+*/
---
+++ Array/Sort in the reverse order/solution.js
@@ -0,0 +1,5 @@
+const sortReverse = arr => arr.sort((a, b) => b - a);
+
+const arr = [2, 5, 3, 4];
+
+const sorted = sortReverse(arr);
