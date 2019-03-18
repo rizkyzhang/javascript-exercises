282ee7bc49359071943536521523663d8ecfdf3c
James868 <41568558+James868@users.noreply.github.com>
2019-03-18 21:10:11 +0700

    Change let to const

--- Array/Map to objects/challenge.js
+++ Array/Map to objects/challenge.js
@@ -4,17 +4,16 @@
 
   Description:
     You have an array of user objects, each one has name, surname and id.
-    Write the code to create another array from it, of objects with id and fullName, where fullName is generated from name and surname.
-    
+    Write the code to create another array from it, of objects with id and fullName, where fullName is generated from name and surname.    
 
   Examples:
-    let john = { name: "John", surname: "Smith", id: 1 };
-    let pete = { name: "Pete", surname: "Hunt", id: 2 };
-    let mary = { name: "Mary", surname: "Key", id: 3 };
+    const john = { name: "John", surname: "Smith", id: 1 };
+    const pete = { name: "Pete", surname: "Hunt", id: 2 };
+    const mary = { name: "Mary", surname: "Key", id: 3 };
 
-    let users = [ john, pete, mary ];
+    const users = [ john, pete, mary ];
 
-    let usersMapped = // ... your code ... //
+    const usersMapped = // ... your code ... //
 
     usersMapped = [
       { fullName: "John Smith", id: 1 },
--- Array/Map to objects/solution.js
+++ Array/Map to objects/solution.js
@@ -1,8 +1,8 @@
-const john = { name: "John", surname: "Smith", id: 1 };
-const pete = { name: "Pete", surname: "Hunt", id: 2 };
-const mary = { name: "Mary", surname: "Key", id: 3 };
+const john = {name: "John", surname: "Smith", id: 1};
+const pete = {name: "Pete", surname: "Hunt", id: 2};
+const mary = {name: "Mary", surname: "Key", id: 3};
 
-const users = [ john, pete, mary ];
+const users = [john, pete, mary];
 
 const usersMapped = users.map(user => ({
   fullname: `${user.name} ${user.surname}`,
