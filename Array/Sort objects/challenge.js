39c9bed033d782f064dd25ab68b0ce3cd7bb1753
James868 <41568558+James868@users.noreply.github.com>
2019-03-19 11:04:55 +0700

    Add an array challenge

---
+++ Array/Sort objects/challenge.js
@@ -0,0 +1,21 @@
+/*
+  Title:
+    Sort objects
+
+  Description:
+    Write the function sortByName(users) that gets an array of objects with the age property and sorts them by age.
+
+  Example:
+    const john = {name: "John", age: 25};
+    const pete = {name: "Pete", age: 30};
+    const mary = {name: "Mary", age: 28};
+
+    const arr = [john, pete, mary];
+
+    sortByName(arr); // now: [john, mary, pete]
+    alert(arr[0].name); // John
+    alert(arr[2].name); // Pete
+
+  Link:
+    http://javascript.info/task/sort-objects
+*/
---
+++ Array/Sort objects/solution.js
@@ -0,0 +1,9 @@
+const sortByAge = arr => arr.sort((a, b) => a.age - b.age);
+
+const john = {name: "John", age: 25};
+const pete = {name: "Pete", age: 30};
+const mary = {name: "Mary", age: 28};
+
+const arr = [john, pete, mary];
+
+sortByAge(arr);
