0650e89ae3736a7b7ab8f54b50fed19bfe859df9
James868 <41568558+James868@users.noreply.github.com>
2019-03-23 12:13:50 +0700

    Add a date challenge

---
+++ Date/Show a weekday/challenge.js
@@ -0,0 +1,14 @@
+/*
+  Title:
+    Show a weekday
+
+  Description:
+    Write a function getWeekDay(date) to show the weekday in short format: ‘MO’, ‘TU’, ‘WE’, ‘TH’, ‘FR’, ‘SA’, ‘SU’.
+
+  Example:
+    let date = new Date(2012, 0, 3);  // 3 Jan 2012
+    alert( getWeekDay(date) );        // should output "TU"
+
+  Link:
+    http://javascript.info/task/get-week-day
+*/
