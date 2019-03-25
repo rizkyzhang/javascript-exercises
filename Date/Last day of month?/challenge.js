35c4a21b4a1904f8b3dfecd8f9285ff345f0b8c9
James868 <41568558+James868@users.noreply.github.com>
2019-03-25 16:46:04 +0700

    Add a date challenge

---
+++ Date/Last day of month?/challenge.js
@@ -0,0 +1,15 @@
+/*
+  Title:
+    Last day of month?
+
+  Description:
+    Write a function getLastDayOfMonth(year, month) that returns the last day of month. Sometimes it is 30th, 31st or even 28/29th for Feb.
+
+    Parameters:
+    year – four-digits year, for instance 2012.
+    month – month, from 0 to 11.
+    For instance, getLastDayOfMonth(2012, 1) = 29 (leap year, Feb).
+
+  Link:
+    http://javascript.info/task/last-day-of-month
+*/
