11519cb80b05bd1f508484000fb8c8ac9ac776b6
James868 <41568558+James868@users.noreply.github.com>
2019-03-26 12:28:43 +0700

    Add a date challenge

---
+++ Date/How many seconds has passed today?/solution.js
@@ -0,0 +1,17 @@
+// Long solution
+
+function getSecondsToday() {
+  const now = new Date();
+  const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  const diff = now - today; // ms difference
+
+  return Math.round(diff / 1000); // convert ms to s
+}
+
+// Short solution
+
+function getSecondsToday() {
+  const now = new Date();
+
+  return now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds();
+}
