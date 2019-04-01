e503e70606f14c07face29dcbd09dee3a620bdd0
James868 <41568558+James868@users.noreply.github.com>
2019-04-01 11:00:47 +0700

    Add a math challenge

---
+++ Math/Sum all numbers till the given one/solution.js
@@ -0,0 +1,19 @@
+// Solution 1 (Loop)
+
+function sumTo(n) {
+  let sum = 0;
+
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
+
+// Solution 2 (Recursion)
+
+const sumTo = n => (n === 1 ? n : n + sumTo(n - 1));
+
+// Solution 3 (Arithmetic Progression Formula)
+
+const sumTo = n => (n * (n + 1)) / 2;
