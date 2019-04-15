44dbb20f29adc84c795b733685e66961a777f131
James868 <41568558+James868@users.noreply.github.com>
2019-04-15 10:58:16 +0700

    Add an array challenge

---
+++ Array/Double an array/challenge.js
@@ -0,0 +1,11 @@
+/*
+  Title: 
+    Double an array
+
+  Description:
+    Given an array, write a function that doubles the array.
+    You could call it like so: double(myArray) or myArray.double().
+
+  Example:
+    Given [1,2,3,4,5], your function should return [1,2,3,4,5,1,2,3,4,5].
+*/
---
+++ Array/Double an array/solution.js
@@ -0,0 +1,29 @@
+// Solution 1
+
+function double(arr) {
+  const result = arr;
+
+  for (let i = 0; i < arr.length; i++) {
+    result.push(arr[i]);
+  }
+
+  return result;
+}
+
+// Solution 2
+
+function double(arr) {
+  const result = arr;
+
+  arr.forEach(elem => result.push(elem));
+
+  return result;
+}
+
+// Solution 3
+
+const double = arr => arr.concat(arr);
+
+// Solution 4
+
+const double = arr => [...arr, ...arr];
