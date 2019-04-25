18215c86c69ac529724fd1abe78ca355fbb73a8c
James868 <41568558+James868@users.noreply.github.com>
2019-04-25 16:02:53 +0700

    Add a string challenge

---
+++ String/Input a string and converts upper case letters to lower and vice versa/solution.js
@@ -0,0 +1,44 @@
+// Solution 1
+
+function swapCase(str) {
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    const letter = str[i];
+    const lowerCase = letter.toLowerCase();
+    const upperCase = letter.toUpperCase();
+
+    letter === lowerCase ? (result += upperCase) : (result += lowerCase);
+  }
+
+  return result;
+}
+
+// Solution 2
+
+function swapCase2(str) {
+  let result = "";
+
+  for (const letter of str) {
+    const lowerCase = letter.toLowerCase();
+    const upperCase = letter.toUpperCase();
+
+    letter === lowerCase ? (result += upperCase) : (result += lowerCase);
+  }
+
+  return result;
+}
+
+// Solution 3
+
+function swapCase3(str) {
+  return str
+    .split("")
+    .map(letter => {
+      const lowerCase = letter.toLowerCase();
+      const upperCase = letter.toUpperCase();
+
+      return letter === lowerCase ? upperCase : lowerCase;
+    })
+    .join("");
+}
