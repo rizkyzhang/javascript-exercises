1dc57b1885b2b74fe9e8000280bcb40e3cb035b3
James868 <41568558+James868@users.noreply.github.com>
2019-05-04 11:58:29 +0700

    Update 'Filter range' example

--- Array/Filter range/challenge.js
+++ Array/Filter range/challenge.js
@@ -11,9 +11,9 @@
 
     const filtered = filterRange(arr, 1, 4);
 
-    alert(filtered); // 3,1 (matching values)
+    console.log(filtered); // -> [3, 1] (matching values)
 
-    alert(arr); // 5,3,8,1 (not modified)
+    console.log(arr); // -> [5, 3, 8, 1] (not modified)
 
   Link:
     http://javascript.info/task/filter-range
