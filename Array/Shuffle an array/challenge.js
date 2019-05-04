b2366562d1481d228d5ea38c79b60a5811277174
James868 <41568558+James868@users.noreply.github.com>
2019-05-04 14:35:52 +0700

    Update 'Shuffle an array' example and solution

--- Array/Shuffle an array/challenge.js
+++ Array/Shuffle an array/challenge.js
@@ -4,21 +4,22 @@
 
   Description:
     Write the function shuffle(array) that shuffles (randomly reorders) elements of the array.
-    Multiple runs of shuffle may lead to different orders of elements. For instance:
+    Multiple runs of shuffle may lead to different orders of elements.
+    All element orders should have an equal probability. 
+    For instance, [1,2,3] can be reordered as [1,2,3] or [1,3,2] or [3,1,2] etc, with equal probability of each case.
 
+  Examples:
     const arr = [1, 2, 3];
 
     shuffleArray(arr);
-    // arr = [3, 2, 1]
+    console.log(arr); // -> [3, 2, 1]
 
     shuffleArray(arr);
-    // arr = [2, 1, 3]
+    console.log(arr); // -> [2, 1, 3]
 
     shuffleArray(arr);
-    // arr = [3, 1, 2]
+    console.log(arr); // -> [3, 1, 2]
 
-    All element orders should have an equal probability. For instance, [1,2,3] can be reordered as [1,2,3] or [1,3,2] or [3,1,2] etc, with equal probability of each case.
-    
   Link:
     http://javascript.info/task/shuffle
 */
--- Array/Shuffle an array/solution.js
+++ Array/Shuffle an array/solution.js
@@ -1,3 +1,5 @@
+// Fisher-Yates Shuffle Algorithm
+
 function shuffleArray(arr) {
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
