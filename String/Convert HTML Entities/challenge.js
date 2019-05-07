48976846a2124c0af26c404243e221e0016b40b1
James868 <41568558+James868@users.noreply.github.com>
2019-05-07 14:39:11 +0700

    Add a string challenge

---
+++ String/Convert HTML Entities/challenge.js
@@ -0,0 +1,17 @@
+/*
+  Title:
+    Convert HTML Entities
+
+  Description:
+    Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+
+  Examples:
+    console.log(convertHtmlEntities("Dolce & Gabbana")); // -> Dolce &amp; Gabbana
+    console.log(convertHtmlEntities("Hamburgers < Pizza < Tacos")); // -> Hamburgers &lt; Pizza &lt; Tacos
+    console.log(convertHtmlEntities("Sixty > twelve")); // -> Sixty &gt; twelve
+    console.log(convertHtmlEntities('Stuff in "quotation marks"')); // -> Stuff in &quot;quotation marks&quot;
+    console.log(convertHtmlEntities("Schindler's List")); // -> Schindler&apos;s List
+
+  Link:
+    https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/convert-html-entities/
+*/
