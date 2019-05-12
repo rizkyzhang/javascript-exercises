04db10a02d1fdb05400b9539bf4ae362a494a2b5
James868 <41568558+James868@users.noreply.github.com>
2019-05-12 08:17:08 +0700

    Update 'Chunky Monkey' solution

--- Array/Chunky Monkey/solution.js
+++ Array/Chunky Monkey/solution.js
@@ -2,10 +2,8 @@ function chunkArrayInGroups(arr, size) {
   const result = [];
   let index = 0;
 
-  for (let i = 0; i < arr.length / size; i++) {
-    result.push(arr.slice(index, index + size));
-
-    index += size;
+  while (index < arr.length) {
+    result.push(arr.slice(index, (index += size)));
   }
 
   return result;
