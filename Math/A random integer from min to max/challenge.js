c00c4cc205baf55f48686379a355e8f6f0156e04
James868 <41568558+James868@users.noreply.github.com>
2019-05-26 10:01:27 +0700

    Update example of random integer and number challenge

--- Math/A random integer from min to max/challenge.js
+++ Math/A random integer from min to max/challenge.js
@@ -6,7 +6,8 @@
     Create a function getRandomInt(min, max) that generates a random integer number from min to max including both min and max as possible values.
 
   Example:
-    console.log(getRandomInt(2, 10)); // -> 4.56
+    console.log(getRandomInt(2, 10)); 
+    // -> 4.56
 
   Link:
     http://javascript.info/task/random-int-min-max
--- Math/A random number from min to max/challenge.js
+++ Math/A random number from min to max/challenge.js
@@ -6,7 +6,8 @@
     Create the function getRandomNum(min, max) to generate a random floating-point number from min to max (not including max).
 
   Example:
-    console.log(getRandomNum(2, 10)); // -> 4.56
+    console.log(getRandomNum(2, 10)); 
+    // -> 4.56
 
   Link:
     http://javascript.info/task/random-min-max
