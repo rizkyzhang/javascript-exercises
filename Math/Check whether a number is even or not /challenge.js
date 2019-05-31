b864f0a4537cd928f80a6db06706009772c67193
James868 <41568558+James868@users.noreply.github.com>
2019-05-31 22:16:34 +0700

    Update 'Check whether a number is even or not' example

--- Math/Check whether a number is even or not /challenge.js
+++ Math/Check whether a number is even or not /challenge.js
@@ -6,8 +6,11 @@
     Create a JavaScript program to check whether a number is even or not.
 
   Examples:
-    console.log(isEvenOrOdd(8)); // -> "even"
-    console.log(isEvenOrOdd(11)); // -> "odd"
+    console.log(isEvenOrOdd(8)); 
+    // -> "even"
+
+    console.log(isEvenOrOdd(11)); 
+    // -> "odd"
 
   Link:
     https://www.w3resource.com/javascript-exercises/javascript-recursion-function-exercise-7.php
