745446a38ba599823368e7bfc9e0dc0fbca60168
James868 <41568558+James868@users.noreply.github.com>
2019-07-02 13:56:04 +0700

    Update solution

--- Array/Easy/Get average age/solution.js
+++ Array/Easy/Get average age/solution.js
@@ -1,7 +1,7 @@
-const john = { name: "John", age: 25 };
-const pete = { name: "Pete", age: 30 };
-const mary = { name: "Mary", age: 29 };
+const john = {name: "John", age: 25};
+const pete = {name: "Pete", age: 30};
+const mary = {name: "Mary", age: 29};
 
 const users = [john, pete, mary];
 
-const getAverageAge = users => users.reduce((prev, user) => prev + user.age, 0);
+const getAverageAge = users => users.reduce((accumulator, user) => accumulator + user.age, 0);
