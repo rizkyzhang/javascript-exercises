e10689c9d43ba4a846730e1fd7e793fd1c97e64c
James868 <41568558+James868@users.noreply.github.com>
2019-07-12 13:11:42 +0700

    Update example

--- String/Easy/Hide email addresses to protect from unauthorized user /challenge.js
+++ String/Easy/Hide email addresses to protect from unauthorized user /challenge.js
@@ -6,7 +6,8 @@
     Create a JavaScript function to hide email addresses to protect from unauthorized user.
 
   Example:
-    console.log(protect_email("robin_singh@example.com")); // -> robin...@example.com
+    console.log(protect_email("robin_singh@example.com")); 
+    // -> robin...@example.com
 
   Link:
     https://www.w3resource.com/javascript-exercises/javascript-string-exercise-6.php
