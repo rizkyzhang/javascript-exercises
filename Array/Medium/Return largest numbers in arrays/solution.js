eae299dfd017fa27527066cb51f1c810905df164
James868 <41568558+James868@users.noreply.github.com>
2019-08-19 13:14:24 +0700

    Add an array challenge

---
+++ Array/Medium/Return largest numbers in arrays/solution.js
@@ -0,0 +1,17 @@
+// Solution 1
+
+function largestOfFour(arrays) {
+  const result = [];
+
+  for (let array of arrays) {
+    result.push(Math.max(...array));
+  }
+
+  return result;
+}
+
+// Solution 2
+
+function largestOfFour(arrays) {
+  return arrays.map(array => Math.max(...array));
+}
