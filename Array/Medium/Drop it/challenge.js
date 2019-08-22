9ef51fadbc4d3d1e3ccd7262654acb876ae020a2
James868 <41568558+James868@users.noreply.github.com>
2019-08-22 10:29:07 +0700

    Update challenge

--- Array/Medium/Drop it/challenge.js
+++ Array/Medium/Drop it/challenge.js
@@ -3,7 +3,7 @@
     Drop it
 
   Description:
-    Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
+    Given an array, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
     Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
 
   Examples:
