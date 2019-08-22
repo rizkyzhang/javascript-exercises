64d0343c8b9cfcc513d27d1ec33b14569472acfb
James868 <41568558+James868@users.noreply.github.com>
2019-08-22 10:53:08 +0700

    Update challenge

--- Array/Medium/Get the symmetric difference between two arrays/challenge.js
+++ Array/Medium/Get the symmetric difference between two arrays/challenge.js
@@ -3,7 +3,7 @@
     Get the symmetric difference between two arrays
 
   Description:
-    Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. 
+    Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both.
     In other words, return the symmetric difference of the two arrays.
 
     Note: You can return the array with its elements in any order.
--- Array/Medium/Return largest numbers in arrays/challenge.js
+++ Array/Medium/Return largest numbers in arrays/challenge.js
@@ -5,7 +5,7 @@
   Description:
     Return an array consisting of the largest number from each provided sub-array. 
     For simplicity, the provided array will contain exactly 4 sub-arrays.
-    Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+    Remember, you can iterate through an array with a simple for loop, and access each element with array syntax arr[i].
 
   Examples:
     console.log(largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
