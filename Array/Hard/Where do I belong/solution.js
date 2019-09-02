f59d29173b7d9ef23e4441a9befe7b0f435eb428
James868 <41568558+James868@users.noreply.github.com>
2019-09-02 15:13:41 +0700

    Update solution

--- Array/Hard/Where do I belong/solution.js
+++ Array/Hard/Where do I belong/solution.js
@@ -1,53 +1,33 @@
 // Solution 1
 
-function getIndexToInsert(arr, num) {
-  if (arr.length === 0) return 0;
+function getIndexToInsert(array, number) {
+  const sortedNumbers = [...array].sort((a, b) => a - b);
 
-  const sortedNums = [...arr].sort((a, b) => a - b);
-
-  for (let i = 0; i < sortedNums.length; i++) {
-    if (sortedNums[i] === num) {
-      return sortedNums.indexOf(arr[i]);
-    }
-  }
-
-  const firstNumLargerThanN = sortedNums.find(currNum => currNum > num);
-
-  if (firstNumLargerThanN === undefined) {
-    return sortedNums.length;
-  }
-
-  return sortedNums.indexOf(firstNumLargerThanN);
-}
-
-// Solution 2
-
-function getIndexToInsert(arr, num) {
-  const sortedNums = [...arr].sort((a, b) => a - b);
-
-  for (let i = 0; i < sortedNums.length; i++) {
-    if (sortedNums[i] >= num) {
+  for (let i = 0; i < sortedNumbers.length; i++) {
+    if (sortedNumbers[i] >= number) {
       return i;
     }
   }
 
-  return sortedNums.length;
+  return sortedNumbers.length;
+}
+
+// Solution 2
+
+function getIndexToInsert(array, number) {
+  const sortedNumbers = [...array].sort((a, b) => a - b);
+  const index = sortedNumbers.findIndexOf(
+    currentNumber => currentNumber >= number
+  );
+
+  return index === -1 ? sortedNumbers.length : index;
 }
 
 // Solution 3
 
-function getIndexToInsert(arr, num) {
-  const sortedNums = [...arr].sort((a, b) => a - b);
-  const index = sortedNums.findIndexOf(currNum => currNum >= num);
-
-  return index === -1 ? sortedNums.length : index;
-}
-
-// Solution 4
-
-function getIndexToInsert(arr, num) {
-  const sortedNums = [...arr].concat(num).sort((a, b) => a - b);
-  const index = sortedNums.indexOf(num);
+function getIndexToInsert(array, number) {
+  const sortedNumbers = [...array].concat(number).sort((a, b) => a - b);
+  const index = sortedNumbers.indexOf(number);
 
   return index;
 }
