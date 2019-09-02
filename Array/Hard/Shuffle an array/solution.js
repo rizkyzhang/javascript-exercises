6924840149007e6c9f003420c3ab5cb5238d7146
James868 <41568558+James868@users.noreply.github.com>
2019-09-02 15:00:54 +0700

    Update solution

--- Array/Hard/Shuffle an array/solution.js
+++ Array/Hard/Shuffle an array/solution.js
@@ -1,8 +1,8 @@
 // Fisher-Yates Shuffle Algorithm
 
-function shuffleArray(arr) {
-  for (let i = arr.length - 1; i > 0; i--) {
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [arr[i], arr[j]] = [arr[j], arr[i]];
+    [array[i], array[j]] = [array[j], array[i]];
   }
 }
