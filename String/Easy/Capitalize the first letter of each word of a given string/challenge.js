8155cf1b2fa95dffb860572e7fcfbe922334553f
James868 <41568558+James868@users.noreply.github.com>
2019-09-25 13:00:00 +0700

    Update challenge

--- String/Easy/Capitalize the first letter of each word of a given string/challenge.js
+++ String/Easy/Capitalize the first letter of each word of a given string/challenge.js
@@ -6,7 +6,8 @@
     Create a JavaScript program to capitalize the first letter of each word of a given string.
 
   Examples:
-    console.log(capitalizeWord("This is cool")); // -> This Is Cool
+    console.log(capitalizeWord("This is cool"));
+    // -> This Is Cool
 
   Link:
     https://www.w3resource.com/javascript-exercises/javascript-basic-exercise-50.php
