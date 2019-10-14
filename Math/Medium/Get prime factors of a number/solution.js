067be807662023657088f116ebff4b9394f2c1ff
James868 <41568558+James868@users.noreply.github.com>
2019-10-14 10:30:43 +0700

    Update solution

--- Math/Medium/Get prime factors of a number/solution.js
+++ Math/Medium/Get prime factors of a number/solution.js
@@ -1,19 +1,21 @@
 function primeFactors(n, minOrMax = null) {
   if (!Number.isInteger(n)) return `${n} is not an integer`;
-  if (Number.isInteger(n) && n < 0) return `${n} is not a positive integer`
 
-  const factors = [];
+  if (n < 0) return `${n} is not a positive integer`
+
+  const primeFactors = [];
 
   for (let i = 2; i <= n; i++) {
     while (n % i === 0) {
-      factors.push(i);
+      primeFactors.push(i);
       n /= i;
     }
   }
 
   if (minOrMax === "min") return Math.min(...factors);
+
   if (minOrMax === "max") return Math.max(...factors);
 
-  return factors.join(" * ");
+  return primeFactors.join(" * ");
 }
 
