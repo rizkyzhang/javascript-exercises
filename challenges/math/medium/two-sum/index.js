f933a4f18c6ed7286fc3626463be8dc033dd2bed
rizkyzhang <41568558+James868@users.noreply.github.com>
2020-01-06 15:18:57 +0700

    feat(math): update two-sum description

--- challenges/math/medium/two-sum/index.js
+++ challenges/math/medium/two-sum/index.js
@@ -1,7 +1,6 @@
 /*
   Description:
     Given an array of integers and a target sum, get all pairs that add up to the target sum. 
-    You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
   Examples:
     console.log(twoSum([7, 0, -4, 5, 2, 3], 5)));
