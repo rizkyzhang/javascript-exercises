57ee723a336904da4fd3621b56ab31d81baac673
rizkyzhang <41568558+James868@users.noreply.github.com>
2020-01-20 11:00:52 +0700

    feat(is-array): update test

--- challenges/array/easy/check-whether-an-input-is-an-array-or-not/test.js
+++ challenges/array/easy/check-whether-an-input-is-an-array-or-not/test.js
@@ -6,18 +6,31 @@ describe("isArray function", () => {
     expect(typeof isArray).toEqual("function");
   });
 
-  test('should return true if passed argument is an array', () => {
+  test('should return true when passed an array', () => {
     expect(isArray([])).toBeTruthy();
     expect(isArray([1, 2, 3])).toBeTruthy();
   });
 
-  test("should return false if passed argument is not an array", () => {
-    expect(isArray("string")).toBeFalsy();
-    expect(isArray("")).toBeFalsy();
-    expect(isArray(123)).toBeFalsy();
-    expect(isArray(undefined)).toBeFalsy();
-    expect(isArray(null)).toBeFalsy();
+  test("should return false when passed an object", () => {
     expect(isArray({})).toBeFalsy();
+    expect(isArray({ "a": 1, "b": 2, "c": 3 }));
+  });
+
+  test("should return false when passed a string", () => {
+    expect(isArray("")).toBeFalsy();
+    expect(isArray("string")).toBeFalsy();
+  });
+
+  test("should return false when passed a number", () => {
+    expect(isArray(123)).toBeFalsy();
+  });
+
+  test("should return false when passed undefined", () => {
+    expect(isArray(undefined)).toBeFalsy();
+  });
+
+  test("should return false when passed null", () => {
+    expect(isArray(null)).toBeFalsy();
   });
 });
 
