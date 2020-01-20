b6fed1e83e2bb891cbb501e41ee9c9e1f7f522b7
rizkyzhang <41568558+James868@users.noreply.github.com>
2020-01-20 11:09:56 +0700

    feat(sort-an-array-of-numbers): update test

--- challenges/array/easy/sort-an-array-of-numbers-in-ascending-order/test.js
+++ challenges/array/easy/sort-an-array-of-numbers-in-ascending-order/test.js
@@ -6,22 +6,21 @@ describe("sortNumAsc function", () => {
     expect(typeof sortNumAsc).toEqual("function");
   });
 
-  test("should return a sorted array of 5 numbers", () => {
+  test("should return a sorted array when passed an array of 5 numbers", () => {
     const array = [-23, 54, -10, -5, 17];
     const sortedArray = [-23, -10, -5, 17, 54];
 
     expect(sortNumAsc(array)).toEqual(sortedArray);
   });
 
-
-  test("should return a sorted array of 10 numbers", () => {
+  test("should return a sorted array when passed an array of 10 numbers", () => {
     const array = [67, -40, 25, 18, -9, -8, 1, 83, 27, -55];
     const sortedArray = [-55, -40, -9, -8, 1, 18, 25, 27, 67, 83];
 
     expect(sortNumAsc(array)).toEqual(sortedArray);
   });
 
-  test("should return a sorted array of 15 numbers", () => {
+  test("should return a sorted array when passed an array of 15 numbers", () => {
     const array = [7, 10, 3, 13, -46, 1, -2, 9, -12, 5, 8, 12, 14, -7, 34];
     const sortedArray = [-46, -12, -7, -2, 1, 3, 5, 7, 8, 9, 10, 12, 13, 14, 34];
 
