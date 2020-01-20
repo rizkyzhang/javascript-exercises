216c760ae7b9a1c210ae90b666e6577214746355
rizkyzhang <41568558+James868@users.noreply.github.com>
2020-01-20 10:59:18 +0700

    feat(is-array): update description

--- challenges/array/easy/check-whether-an-input-is-an-array-or-not/index.js
+++ challenges/array/easy/check-whether-an-input-is-an-array-or-not/index.js
@@ -1,6 +1,6 @@
 /*
   Description:
-    Create a function to check whether an input is an array or not.
+    Check whether input is an array or not.
     In JavaScript, array are not a true array, they are actually an object.
     So you can’t simply do a typeof check, because it will return an object.
  
@@ -19,9 +19,6 @@
 
     console.log(isArray([1, 2, 3]));
     // -> true
-
-  Link:
-    https://www.w3resource.com/javascript-exercises/javascript-array-exercise-1.php
 */
 
 function isArray(dataType) {
