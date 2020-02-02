9510b24fd17297258a99149d0b7fbc43fa89b1b6
rizkyzhang <41568558+rizkyzhang@users.noreply.github.com>
2020-02-02 19:01:03 +0700

    feat(split-an-array-into-subarrays): update description

--- challenges/array/hard/split-an-array-into-subarrays/index.js
+++ challenges/array/hard/split-an-array-into-subarrays/index.js
@@ -1,7 +1,7 @@
 /*
   Description:
-    Given an array and an integer, create a function that splits an array (first argument) into subarrays of N length (second argument).
-    and returns them as a two-dimensional array.
+    Given an array and an integer length, split the array into subarrays of N length.
+    and return them as a two-dimensional array.
 
   Examples:
     console.log(splitArray(["a", "b", "c", "d"], 2)); 
@@ -12,19 +12,7 @@
 */
 
 function splitArray(array, length) {
-  if (length < 1) {
-    return "Length must be more than 0";
-  }
 
-  const arrays = [];
-  const totalElements = array.length;
-  let sliceIndex = 0;
-
-  while (sliceIndex < totalElements) {
-    arrays.push(array.slice(sliceIndex, sliceIndex += length));
-  }
-
-  return arrays;
 }
 
 module.exports = splitArray;
