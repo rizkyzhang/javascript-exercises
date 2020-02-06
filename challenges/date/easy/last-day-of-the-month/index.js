0d20e7070a95bd1d7484b86d4bfdd7d8dafb57ea
rizkyzhang <41568558+rizkyzhang@users.noreply.github.com>
2020-02-06 10:45:04 +0700

    feat(last-day-of-the-month): rename challenge

---
+++ challenges/date/easy/last-day-of-the-month/index.js
@@ -0,0 +1,20 @@
+/*
+  Description:
+    Given two integers, year (4 digits year) and month (0 - 11), create a function that gets the last day of month.
+    Sometimes it is 30th, 31st or even 28/29th for February.
+
+  Examples:
+    console.log(getLastDayOfMonth(2019, 1));
+    // -> 28
+
+    console.log(getLastDayOfMonth(2019, 11));
+    // -> 31     
+*/
+
+
+function getLastDayOfMonth(year, month) {
+
+}
+
+module.exports = getLastDayOfMonth;
+
