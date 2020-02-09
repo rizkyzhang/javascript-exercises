2891e7ab2577a2b39c562b40a049aba0d69a17a1
rizkyzhang <41568558+rizkyzhang@users.noreply.github.com>
2020-02-09 08:52:53 +0700

    feat(is-even): update description

---
+++ challenges/math/easy/is-even/index.js
@@ -0,0 +1,18 @@
+/*
+  Description:
+    Given a integer n, check whether it is even or not.
+
+  Examples:
+    console.log(isEvenOrOdd(8));
+    // -> even
+
+    console.log(isEvenOrOdd(11));
+    // -> odd
+*/
+
+function isEvenOrOdd() {
+
+}
+
+module.exports = isEvenOrOdd;
+
