650c5ed2aea01cf2dc7da9a989fd301dafeeb93e
rizkyzhang <41568558+rizkyzhang@users.noreply.github.com>
2020-02-09 08:52:14 +0700

    feat(fizzbuzz): update description

--- challenges/math/easy/fizzbuzz/index.js
+++ challenges/math/easy/fizzbuzz/index.js
@@ -1,6 +1,6 @@
 /*
   Description:
-    Given an integer n, create a function that prints each number from 1 to n on a new line.
+    Given an integer n, prints each number from 1 to n on a new line.
     For each multiple of 3, print "Fizz" instead of the number.
     For each multiple of 5, print "Buzz" instead of the number.
     For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
