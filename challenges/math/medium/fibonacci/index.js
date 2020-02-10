143897e72d5c866a5081930323fbb95edd13586e
rizkyzhang <41568558+rizkyzhang@users.noreply.github.com>
2020-02-10 09:43:38 +0700

    feat(fibonacci): update description

--- challenges/math/medium/fibonacci/index.js
+++ challenges/math/medium/fibonacci/index.js
@@ -1,6 +1,6 @@
 /*
   Description:
-    Given an integer n, create a function that gets the first n fibonacci numbers.
+    Given an integer n, get the first n fibonacci numbers.
     The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, . . . Each subsequent number is the sum of the previous two.
 
   Examples:
