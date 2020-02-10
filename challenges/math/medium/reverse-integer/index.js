f4b17f3e04b544900571af6a559ab1de0cc6ed82
rizkyzhang <41568558+rizkyzhang@users.noreply.github.com>
2020-02-10 18:42:58 +0700

    feat(reverse-integer): update description

--- challenges/math/medium/reverse-integer/index.js
+++ challenges/math/medium/reverse-integer/index.js
@@ -1,6 +1,6 @@
 /*
   Description:
-    Given a 32-bit signed integer, create a function that reverses it.
+    Given a 32-bit signed integer, reverse it.
     Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1].
     For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 
