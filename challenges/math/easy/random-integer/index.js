300d553ab8b882a2f5cb32d3a9d9750e97c293fe
rizkyzhang <41568558+rizkyzhang@users.noreply.github.com>
2020-02-11 12:23:58 +0700

    feat(random-integer): rename challenge

---
+++ challenges/math/easy/random-integer/index.js
@@ -0,0 +1,15 @@
+/*
+  Description
+    Given two integers, min and max, generate a random integer from min to max.
+
+  Example:
+    console.log(generateRandomInteger(2, 10)); 
+    // -> 4
+*/
+
+function generateRandomInteger(min, max) {
+
+}
+
+module.exports = generateRandomInteger;
+
