68ede43484a8e5b9e836dc3e1e942b14c436dc2c
rizkyzhang <41568558+rizkyzhang@users.noreply.github.com>
2020-06-14 20:01:02 +0700

    feat(merge-arrays): update description and examples

--- challenges/array/medium/merge-arrays/index.js
+++ challenges/array/medium/merge-arrays/index.js
@@ -1,13 +1,13 @@
 /*
   Description:
-    Given two or more arrays, merge it without any duplicate elements.
+    Given two or more arrays, merge it into one array.
 
   Examples:
     console.log(mergeArrays([1, 2, 3, 4, 5], [6, 2, 10, 4, 3]));
-    // -> [1, 2, 3, 4, 5, 6, 10]
+    // -> [1, 2, 3, 4, 5, 6, 2, 10, 4, 3]
 
     console.log(mergeArrays(["a", "b"],  ["c", "d"], ["f, "b", "c"], ["g", "h"]));
-    // -> ["a", "b", "c", "d", "e", "f", "g", "h"]
+    // -> ["a", "b", "c", "d", "f", "b", "c", "g", "h"]
 */
 
 function mergeArrays(arrays) {
